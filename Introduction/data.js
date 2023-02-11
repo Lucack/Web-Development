3cac329d5a25cd5689140c49646f19ddd71e72c0
Lucack <lucak260@gmail.com>
2023-02-11 19:00:03 -0300

    Learning Java Script

---
+++ Introduction/data.js
@@ -0,0 +1,14 @@
+var nome = "Caneta";
+var quantidade = 10;
+var preço = 6.4;
+var imposto = 1.5;
+let preçoFinal = preço + imposto;
+
+console.log(nome);
+console.log(quantidade);
+console.log(preço);
+console.log(imposto);
+console.log(preçoFinal);
+
+nome = "Caneta Bic";
+console.log(nome);
---
+++ Introduction/set2.js
@@ -0,0 +1,19 @@
+console.log('Hello'); console.log(1+3*5); console.log("Hello");
+// comentário
+
+/*
+
+console.log('Hello'); 
+console.log(1+3*5);
+console.log("Hello");
+
+*/
+
+{ 
+    // bloco de codigo
+    console.log('Hello'); 
+    console.log(1+3*5); 
+    {
+        console.log("Hello");
+    }
+}
